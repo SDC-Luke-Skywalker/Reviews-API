e19dd6b4cf60148354a69118256a7ba131f966a0
Jacob Gramer <grameraggie@gmail.com>
2022-09-16 16:49:33 -0500

    Add Server folder w/ basic express server in index.js

--- Server/index.js
+++ Server/index.js
@@ -3,3 +3,9 @@ const express = require('express');
 const axios = require('axios');
 
 const app = express();
+const PORT = process.env.PORT || 5432;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.listen(PORT, () => console.log(`Listending on port ${PORT}`));
