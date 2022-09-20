737223c5e6cb3e69677aeac5c3177df4aa56602d
Jacob Gramer <grameraggie@gmail.com>
2022-09-19 22:16:50 -0500

    Add indices to all tables

--- Database/postgres_schemas.sql
+++ Database/postgres_schemas.sql
@@ -24,6 +24,12 @@ COPY reviews(review_id, product_id, rating, date, summary, body, recommend, repo
 DELIMITER ','
 CSV HEADER;
 
+CREATE INDEX reviews_product_id_index
+ON reviews (product_id);
+
+CREATE INDEX ratings_index
+ON reviews (rating);
+
 CREATE TABLE photos (
   photo_id int,
   review_id int,
@@ -36,6 +42,9 @@ COPY photos(photo_id, review_id, url) FROM '/Users/jacobgramer/HackReactor/Ratin
 DELIMITER ','
 CSV HEADER;
 
+CREATE INDEX photos_review_id_index
+ON photos (review_id);
+
 CREATE TABLE characteristics (
   characteristic_id int,
   product_id int,
@@ -46,6 +55,9 @@ CREATE TABLE characteristics (
 COPY characteristics(characteristic_id, product_id, name) FROM '/Users/jacobgramer/HackReactor/Ratings-Reviews-API/characteristics.csv'
 DELIMITER ','
 CSV HEADER;
+
+CREATE INDEX characteristics_product_id
+ON characteristics (product_id);
 
 CREATE TABLE rev_characteristics (
   rev_char_id int,
@@ -60,3 +72,6 @@ CREATE TABLE rev_characteristics (
 COPY rev_characteristics(rev_char_id, characteristic_id, review_id, value) FROM '/Users/jacobgramer/HackReactor/Ratings-Reviews-API/characteristic_reviews.csv'
 DELIMITER ','
 CSV HEADER;
+
+CREATE INDEX rev_characteristics_characteristic_id
+ON rev_characteristics (characteristic_id);
