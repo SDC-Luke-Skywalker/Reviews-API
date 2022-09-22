aae040f5b1f8bfb194585f4d7dbebeeefaa6fd30
Ubuntu <ubuntu@ip-172-31-3-234.us-east-2.compute.internal>
2022-09-22 00:22:01 +0000

    update .env files to match correct username and password

--- Server/index.js
+++ Server/index.js
@@ -11,6 +11,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
+app.get('/loaderio-5a18b532c1cd19e2669c4fa17c1f287b.txt', (req, res) => (res.status(200).send('loaderio-5a18b532c1cd19e2669c4fa17c1f287b'))); 
+
 app.get('/reviews/:product_id/list', (req, res) => {
   controller.getReviews(req, res);
 });
